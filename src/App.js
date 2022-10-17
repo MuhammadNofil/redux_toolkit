76dbfafd2a6880e7bba1c052934c7adc5603021c
MuhammadNofil <muhammadnofil1290@gmail.com>
2022-10-17 21:39:24 +0500

    push by nofil

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import {increment,decrement} from './redux/counter'
+import { useDispatch, useSelector } from 'react-redux';
 function App() {
+  const {count}=useSelector((state)=>state.counter)
+  const dispatch=useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>the count is :{count}</h1>
+      <button onClick={()=>dispatch(increment())}>incriment</button>
+      <button onClick={()=>dispatch(decrement())}>decriment</button>
     </div>
   );
 }
